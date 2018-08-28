e36c7fb68c1ba168976dd999b4a403db31d3a8f7
SheryLanado <42298615+SheryLanado@users.noreply.github.com>
2018-08-28 18:57:51 -0500

    commit

--- resources/views/public/es/index.blade.php
+++ resources/views/public/es/index.blade.php
@@ -530,15 +530,15 @@ input[type=radio]:checked + label>img {
                           <li class="col-sm-3">      
                             <div class="thumbnail">
                                  @if (empty($item->photo))
-                              <a href="#"><img src="/assets/public/images/sight/5.jpg" alt=""></a>
+                              <a href="#"><img src="/assets/content/testimonio/default.png" alt=""></a>
                               @else
                               <a href="#"><img src="/assets/content/testimonio/{{$item->id}}.{{$item->photo}}" alt=""></a>
                               @endif
                             </div>
                             <div class="caption-box" style="text-align: center;">
                               <h3>{{$item->name}} - {{$item->nationality}}</h3>
-                              <h5>{{$item->testimonial}}</h5>
-                              <h5>{{$item->email}} <h5>
+                              <h5 style="text-align: justify;">{!!str_limit($item->testimonial, 150)!!}</h5>
+                             
                               <h5>{{$item->date}} <h5>
                               <a class="link-btn" href="/es/testimonials">Leer Más</a>
                                <div class="tour_head">
