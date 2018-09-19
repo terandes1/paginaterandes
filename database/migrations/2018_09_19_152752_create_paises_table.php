5da5ec9a4edb6c3f9f792e5b9c5cbc9a16d79d48
michael <michael101136@gmail.com>
2018-09-19 15:32:23 -0500

    creacion de trablas de paises
    creacion de trablas de paises

---
+++ database/migrations/2018_09_19_152752_create_paises_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paises', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('codigo',120)->nullable();
+            $table->string('pais',120)->nullable();
+            $table->string('phone_code',120)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paises');
+    }
+}
